83132b0a60b51d79136d7bc125e29951d3f0f11e
Chris Howe-Jones <chris.howejones@gmail.com>
2018-02-25 09:56:46 +0000

    Added fn to add an artist.

--- src/graphql_example_clojure/db.clj
+++ src/graphql_example_clojure/db.clj
@@ -1,8 +1,18 @@
 (ns graphql-example-clojure.db
-  (:require [datomic.api :as d]
-            [com.stuartsierra.component :as component]))
+  (:require [clojure.string :as s]
+            [com.stuartsierra.component :as component]
+            [datomic.api :as d]))
 
 (def uri "datomic:dev://localhost:4334/mbrainz-1968-1973")
+
+(defn get-schema-attributes
+  [db]
+  (d/q '[:find [?ident ...]
+         :where
+         [?a :db/ident ?ident]
+         [_ :db.install/attribute ?a]
+         (not [?a :db/valueType :db.type/ref])]
+       db))
 
 (defn artist-by-name
   [db name]
@@ -83,22 +93,91 @@
            format-id)
       name))
 
+(defn- add-ns
+  [entity-map ns]
+  (->  entity-map
+       keys
+       (->> (map #(->> % name (str ns "/") keyword)))
+       (zipmap (vals entity-map))))
+
+(defn- build-valid-artist
+  [artist-attrs artist-with-ns]
+  (reduce (fn [m [k v]] (if (contains? artist-attrs k) (assoc m k v) m))
+          {}
+          artist-with-ns))
+
+(defn add-artist
+  [conn db-schema-attrs artist]
+  (let [artist-with-ns (add-ns artist "artist")
+        artist-attrs (set (filter #(= "artist" (namespace %)) db-schema-attrs))
+        valid-artist (build-valid-artist artist-attrs artist-with-ns)
+        id-map {:db/id #db/id [:db.part/user]}
+        tx-data (merge valid-artist id-map)]
+   (d/transact conn [tx-data])))
+
 (defrecord Database
     [db-uri]
   component/Lifecycle
   (start [this]
     (when db-uri
-      (assoc this :db-conn (d/connect db-uri))))
+      (let [conn (d/connect db-uri)]
+        (-> this
+            (assoc :db-conn conn)
+            (assoc :db-schema-attrs (get-schema-attributes (d/db conn)))))))
   (stop [this]
     (when-let [conn (:db-conn this)]
       (d/release conn))
-    (assoc this :db-conn nil)))
+    (-> this
+        (assoc :db-conn nil)
+        (assoc :db-schema-attrs nil))))
 
 (defn new-database []
   {:db (component/using (map->Database {})
                         [:db-uri])})
 
 (comment
+
+  (let [artist {:artist/name "Chris" :artist/country 1234}
+        valid-attrs #{:artist/name :artist/age}]
+    (reduce (fn [m [k v]] (if (contains? valid-attrs k) (assoc m k v) m))
+            {}
+            artist))
+
+  (add-artist (d/connect uri)
+              (get-schema-attributes (d/db (d/connect uri)))
+              {:name "Chris" :startYear 1981 :startMonth 4 :startDay 2
+                               :country 17592186045580})
+
+  (d/q '[:find (pull ?e [*]) :where [?e :country/name "United Kingdom"]] (d/db (d/connect uri)))
+
+  (d/q '[:find ?e . :where [?e :db/ident :country/GB]] (d/db (d/connect uri)))
+
+  (d/transact (d/connect uri) (map
+                               #(cons :db.fn/retractEntity %)
+                               (d/q '[:find ?e
+                                      :in $
+                                      :where
+                                      [?e :artist/name "Chris"]]
+                                    (d/db (d/connect uri)))))
+
+  (map
+   #(cons :db.fn/retractEntity %)
+   (d/q '[:find ?e
+          :in $
+          :where
+          [?e :artist/name "Chris"]]
+        (d/db (d/connect uri))))
+
+  (artist-by-name (d/db (d/connect uri)) "Chris")
+
+  (d/q '[:find  (pull ?a [*])
+         :in $ ?artist-name
+         :where
+         [?a :artist/name ?artist-name]]
+       (d/db (d/connect uri))
+       "Chris")
+
+
 
   (release-by-name "Hot Rocks 1964-1971")
 
@@ -143,7 +222,4 @@
             :where
             [?a :artist/name ?artist-name]]
           (db)
-          "The Rolling Stones"))))
-
-
-  )
+          "The Rolling Stones")))))
